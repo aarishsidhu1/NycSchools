3c4967b83e5c8f752bb1af31c957c4fcee7cccf7
aarishsidhu <aarishsidhu6@gmail.com>
2023-02-15 16:18:10 -0600

    Changes to GET

--- app/src/main/java/us/aarishsidhu/a20230208_aarishpreetsinghsidhu_nycschools/network/api/ApiInterface.java
+++ app/src/main/java/us/aarishsidhu/a20230208_aarishpreetsinghsidhu_nycschools/network/api/ApiInterface.java
@@ -10,7 +10,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
-    @GET("s3k6-pzi2.json?$limit=10")
+    @GET("s3k6-pzi2.json?$limit=200")
     Call<List<School>> doListOfSchools();
 
     @GET("f9bf-2cp4.json?")
